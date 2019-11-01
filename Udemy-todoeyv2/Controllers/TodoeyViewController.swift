5b6c2f7d68053293c7b48e925e0ba3db17e4997a
Dawn E Law <dawnelaw@gmail.com>
2019-11-01 14:55:14 -0600

    latest plist version mvc

--- Udemy-todoeyv2/Controllers/TodoeyViewController.swift
+++ Udemy-todoeyv2/Controllers/TodoeyViewController.swift
@@ -12,28 +12,31 @@ class TodoeyViewController: UITableViewController {
     
     var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+        //let defaults = UserDefaults.standard
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+
+        
+        print(dataFilePath)
       
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "Buy Eggos"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Destroy Demogoron"
-        itemArray.append(newItem3)
-        
-        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-           itemArray = items
-        }
-        
+//        let newItem = Item()
+//        newItem.title = "Find Mike"
+//        itemArray.append(newItem)
+//
+//        let newItem2 = Item()
+//        newItem2.title = "Buy Eggos"
+//        itemArray.append(newItem2)
+//
+//        let newItem3 = Item()
+//        newItem3.title = "Destroy Demogoron"
+//        itemArray.append(newItem3)
+
+        loadItems()
         
     }
 
@@ -67,7 +70,7 @@ class TodoeyViewController: UITableViewController {
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         //Set to opposite
         
-        tableView.reloadData()  //call data respurce method again
+        saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
         
@@ -88,10 +91,10 @@ class TodoeyViewController: UITableViewController {
             newItem.title = textField.text!
             
             self.itemArray.append(newItem)
-                        
-            self.defaults.set(self.itemArray, forKey:"TodoListArray")
             
-            self.tableView.reloadData()
+            self.saveItems()
+            
+
         }
         
         alert.addTextField{ (alertTextField) in
@@ -103,6 +106,34 @@ class TodoeyViewController: UITableViewController {
             
         present(alert, animated: true, completion: nil)
             
+    }
+    
+ //MARK - Model Manipualtion Methods
+
+    func saveItems() {
+
+         let encoder = PropertyListEncoder()
+         do {
+             let data = try encoder.encode(itemArray)
+             try data.write(to: dataFilePath!)
+         } catch {
+             print("Error encoding item array, \(error)")
+         }
+        
+         self.tableView.reloadData()
+        
+    }
+    
+    func loadItems() {
+        
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+        }
     }
 }
 
